8406986a5942aeaea6e636d1cadbe7aa92ef7c8e
Brian Bai <1980337991@qq.com>
2024-10-25 01:29:30 +0800

    添加js点击功能

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-const list = document.createElement("ul");
-const info = document.createElement("p");
-const html = document.querySelector("html");
+document.addEventListener("DOMContentLoaded", (event) => {
+  const signUpButton = document.querySelector(".sign_up_button");
 
-info.textContent =
-  "Below is a dynamic list. Click anywhere on the page to add a new list item. Click an existing list item to change its text to something else.";
+  signUpButton.addEventListener("click", change_style);
 
-document.body.appendChild(info);
-document.body.appendChild(list);
-
-html.onclick = function () {
-  const listItem = document.createElement("li");
-  const listContent = prompt("What content do you want the list item to have?");
-  listItem.textContent = listContent;
-  list.appendChild(listItem);
-
-  listItem.onclick = function (e) {
-    e.stopPropagation();
-    const listContent = prompt("Enter new content for your list item");
-    this.textContent = listContent;
-  };
-};
+  function change_style() {
+    signUpButton.classList.toggle("click_active");
+  }
+});
